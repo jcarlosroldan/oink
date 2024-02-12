3680327708185ad8b9cb3d1421e99909f318d8e7
Carlos Roldán <juancarlos@sevilla.es>
2024-02-12 12:45:10 +0100

    Moved blog to basic

--- examples/basic/db.php
+++ examples/basic/db.php
@@ -5,34 +5,35 @@ namespace DB;
 $_conn = null;
 function conn() {
 	global $_conn;
-	if ($_conn) return $_conn;
-	$populate = !file_exists("data.db");
-	$_conn = new \PDO("sqlite:data.db");
-	if ($populate) {
-		$_conn->exec("
-			CREATE TABLE posts (
-				id INTEGER PRIMARY KEY,
-				title TEXT,
-				body TEXT,
-				tag TEXT,
-				created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-			);
-			CREATE TABLE comments (
-				id INTEGER PRIMARY KEY,
-				post_id INTEGER,
-				author TEXT,
-				text TEXT,
-				created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-			);
-			INSERT INTO posts (title, body, tag) VALUES
-				('Hello, world!', 'This is the first post.', 'foo'),
-				('Second post', 'This is the second post.', 'foo'),
-				('Third post', 'This is the third post.', 'bar');
-			INSERT INTO comments (post_id, author, text) VALUES
-				(1, 'author1@mail.com', 'This is the first comment.'),
-				(1, 'author2@mail.com', 'This is the second comment.'),
-				(2, 'author3@mail.com', 'This is the third comment.');
-		");
+	if ($_conn === null) {
+		$populate = !file_exists("data.db");
+		$_conn = new \PDO("sqlite:data.db");
+		if ($populate) {
+			$_conn->exec("
+				CREATE TABLE posts (
+					id INTEGER PRIMARY KEY,
+					title TEXT,
+					body TEXT,
+					tag TEXT,
+					created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+				);
+				CREATE TABLE comments (
+					id INTEGER PRIMARY KEY,
+					post_id INTEGER,
+					author TEXT,
+					text TEXT,
+					created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+				);
+				INSERT INTO posts (title, body, tag) VALUES
+					('Hello, world!', 'This is the first post.', 'foo'),
+					('Second post', 'This is the second post.', 'foo'),
+					('Third post', 'This is the third post.', 'bar');
+				INSERT INTO comments (post_id, author, text) VALUES
+					(1, 'author1@mail.com', 'This is the first comment.'),
+					(1, 'author2@mail.com', 'This is the second comment.'),
+					(2, 'author3@mail.com', 'This is the third comment.');
+			");
+		}
 	}
 	return $_conn;
 }
--- examples/basic/index.php
+++ examples/basic/index.php
@@ -1,9 +1,5 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 include_once '../../oink.php';
 
 Oink\serve('endpoints.php', base_path: '/examples/blog', allow_get: true);
